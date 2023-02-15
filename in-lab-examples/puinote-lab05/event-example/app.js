c72dcbdebaa03eabe536d6ed6d2dfda623424974
eunilee2 <115505438+eunilee2@users.noreply.github.com>
2023-02-14 19:24:56 -0500

    update? js

---
+++ in-lab-examples/puinote-lab05/event-example/app.js
@@ -0,0 +1,8 @@
+class Notecard{
+    constructor(imageURL, title, body){
+        this.noteImageURL=imageURL;
+        this.noteTitle=title;
+        this.noteBody=body;
+        this.element=null;
+    }
+}
